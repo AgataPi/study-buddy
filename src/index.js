08b3fb8a1ea5676e8c93d0fa8c4419c50af4f8fe
Agata Piątek <a.piatek100@gmail.com>
2022-02-01 15:27:14 +0100

    Fix Google fonts bug

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import 'index.css';
 import Root from 'view/Root';
+import 'assets/styles/fonts.css';
 
 ReactDOM.render(
   <React.StrictMode>
