002cf849bb994ea985765fab80aad2ba7aaebebf
Agata Piątek <a.piatek100@gmail.com>
2022-02-04 14:44:53 +0100

    Forms and routing final

--- src/view/Root.js
+++ src/view/Root.js
@@ -1,17 +1,67 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+import { users as usersData } from 'data/users';
 import UsersList from 'components/organisms/UsersList/UsersList';
+import Form from 'components/organisms/Form/Form';
 import { ThemeProvider } from 'styled-components';
 import { GlobalStyle } from 'assets/styles/GlobalStyle';
 import { theme } from 'assets/styles/theme';
 import { Wrapper } from './Root.styles';
 
-const Root = () => (
-  <ThemeProvider theme={theme}>
-    <GlobalStyle />
-    <Wrapper>
-      <UsersList />
-    </Wrapper>
-  </ThemeProvider>
-);
+const initialFormState = {
+  name: '',
+  attendance: '',
+  average: '',
+};
+const Root = () => {
+  const [users, setUsers] = useState(usersData);
+  const [formValues, setFormValues] = useState(initialFormState);
+
+  const deleteUser = (name) => {
+    const filteredUsers = users.filter((user) => user.name !== name);
+    setUsers(filteredUsers);
+  };
+
+  const handleInputChange = (e) => {
+    setFormValues({
+      ...formValues,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const handleAddUser = (e) => {
+    e.preventDefault();
+    const newUser = {
+      name: formValues.name,
+      attendance: formValues.attendance,
+      average: formValues.average,
+    };
+
+    setUsers([newUser, ...users]);
+    setFormValues(initialFormState);
+  };
+
+  return (
+    <Router>
+      <ThemeProvider theme={theme}>
+        <GlobalStyle />
+        <Wrapper>
+          <nav>
+            <Link to="/">Home</Link>
+            <Link to="/add-user">Add user</Link>
+          </nav>
+          <Switch>
+            <Route path="/" exact>
+              <UsersList deleteUser={deleteUser} users={users} />
+            </Route>
+            <Route path="/add-user">
+              <Form handleAddUser={handleAddUser} handleInputChange={handleInputChange} formValues={formValues} />
+            </Route>
+          </Switch>
+        </Wrapper>
+      </ThemeProvider>
+    </Router>
+  );
+};
 
 export default Root;
