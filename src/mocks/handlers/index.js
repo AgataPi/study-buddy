44902ab207c68ae47e4e2624e27a3ba83acebd9f
Agata Piątek <a.piatek100@gmail.com>
2022-03-07 15:36:37 +0100

    Modal final

--- src/mocks/handlers/index.js
+++ src/mocks/handlers/index.js
@@ -4,8 +4,56 @@ import { groups } from 'mocks/data/groups';
 
 export const handlers = [
   rest.get('/groups', (req, res, ctx) => {
-    return res(ctx.status(200), ctx.json({ groups }));
+    return res(
+      ctx.status(200),
+      ctx.json({
+        groups,
+      })
+    );
   }),
+  rest.get('/groups/:id', (req, res, ctx) => {
+    if (req.params.id) {
+      const matchingStudents = students.filter((student) => student.group === req.params.id);
+      return res(
+        ctx.status(200),
+        ctx.json({
+          students: matchingStudents,
+        })
+      );
+    }
+    return res(
+      ctx.status(200),
+      ctx.json({
+        students,
+      })
+    );
+  }),
+  rest.get('/students/:id', (req, res, ctx) => {
+    if (req.params.id) {
+      const matchingStudents = students.find((student) => student.id === req.params.id);
+      if (!matchingStudents) {
+        return res(
+          ctx.status(404),
+          ctx.json({
+            error: 'No matching student',
+          })
+        );
+      }
+      return res(
+        ctx.status(200),
+        ctx.json({
+          students: matchingStudents,
+        })
+      );
+    }
+    return res(
+      ctx.status(200),
+      ctx.json({
+        students,
+      })
+    );
+  }),
+
   rest.post('/students/search', (req, res, ctx) => {
     const matchingStudents = req.body.searchPhrase
       ? students.filter((student) => student.name.toLowerCase().includes(req.body.searchPhrase.toLowerCase()))
@@ -14,24 +62,6 @@ export const handlers = [
       ctx.status(200),
       ctx.json({
         students: matchingStudents,
-      })
-    );
-  }),
-  rest.get('/students/:group', (req, res, ctx) => {
-    if (req.params.group) {
-      const matchingStudents = students.filter((student) => student.group === req.params.group);
-      return res(
-        ctx.status(200),
-        ctx.json({
-          students: matchingStudents,
-        })
-      );
-    }
-
-    return res(
-      ctx.status(200),
-      ctx.json({
-        students,
       })
     );
   }),
--- src/views/Dashboard.js
+++ src/views/Dashboard.js
@@ -4,18 +4,28 @@ import StudentsList from 'components/organisms/StudentsList/StudentsList';
 import { useStudents } from 'hooks/useStudents';
 import { GroupWrapper, Wrapper, TitleWrapper } from './Dashboard.style';
 import { Title } from 'components/atoms/Title/Title';
+import useModal from 'components/organisms/Modal/useModal';
+import StudentDetails from 'components/molecules/StudentDetails/StudentDetails';
 
 const Dashboard = () => {
   const [groups, setGroups] = useState([]);
-  const { getGroups } = useStudents();
+  const [currentStudent, setCurrentStudent] = useState([]);
+  const { getGroups, getStudentById } = useStudents();
   const { id } = useParams();
+  const { Modal, isOpen, handleOpenModal, handleCloseModal } = useModal();
 
   useEffect(() => {
     (async () => {
       const groups = await getGroups();
       setGroups(groups);
     })();
-  }, []);
+  }, [getGroups]);
+
+  const handleOpenStudentDetails = async (id) => {
+    const student = await getStudentById(id);
+    setCurrentStudent(student);
+    handleOpenModal();
+  };
 
   if (!id && groups.length > 0) return <Redirect to={`/group/${groups[0]}`} />;
 
@@ -32,7 +42,12 @@ const Dashboard = () => {
         </nav>
       </TitleWrapper>
       <GroupWrapper>
-        <StudentsList />
+        <StudentsList handleOpenStudentDetails={handleOpenStudentDetails} />
+        {isOpen ? (
+          <Modal handleClose={handleCloseModal}>
+            <StudentDetails student={currentStudent} />
+          </Modal>
+        ) : null}
       </GroupWrapper>
     </Wrapper>
   );
