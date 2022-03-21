f59964c02cb9718dac03b4f8ddbcffe86f5b601d
Agata Piątek <a.piatek100@gmail.com>
2022-03-21 12:52:19 +0100

    Errors final

--- src/providers/AppProviders.js
+++ src/providers/AppProviders.js
@@ -4,15 +4,18 @@ import { theme } from 'assets/styles/theme';
 import { GlobalStyle } from 'assets/styles/GlobalStyle';
 import { AuthProvider } from 'hooks/useAuth';
 import { BrowserRouter as Router } from 'react-router-dom';
+import { ErrorProvider } from 'hooks/useError';
 
 const AppProviders = ({ children }) => {
   return (
     <Router>
       <ThemeProvider theme={theme}>
-        <AuthProvider>
-          <GlobalStyle />
-          {children}
-        </AuthProvider>
+        <ErrorProvider>
+          <AuthProvider>
+            <GlobalStyle />
+            {children}
+          </AuthProvider>
+        </ErrorProvider>
       </ThemeProvider>
     </Router>
   );
